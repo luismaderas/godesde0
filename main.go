cde31b7ddd19b1e75954eee09f8af904a12f549f
unknown <luismaderas@gmail.com>
2025-05-02 17:21:21 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/luismaderas/variables"
 )
 
 func main() {
 	//fmt.Println("Hola Mundo")//
-	variables.MuestraEnteros()
-
+	//variables.MuestraEnteros()//
+	//variables.RestoVariables()//
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
